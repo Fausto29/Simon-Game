8bc766d62be07b1ad192bdf028af38c956eea81f
Blaze <Blaze@192.168.1.44>
2019-06-24 22:08:42 +0200

    finalizado el algoritmo de reproduccion de secuencia

--- js/botones.js
+++ js/botones.js
@@ -145,3 +145,50 @@ document.getElementById('boton3').onclick=function(){
 	}else {
 		indice++;}
 }
+function zero (){
+	var objAudio = document.getElementById('audioBoton0');
+	objAudio.play();
+	var objBoton = document.getElementById('boton0').src="img/boton0.png";
+}
+
+function uno (){
+	var objAudio = document.getElementById('audioBoton1');
+	objAudio.play();
+	var objBoton = document.getElementById('boton1').src="img/boton1.png";
+
+}
+
+function dos (){
+	var objAudio = document.getElementById('audioBoton2');
+	objAudio.play();
+	var objBoton = document.getElementById('boton2').src="img/boton2.png";
+}
+
+function tres (){
+	var objAudio = document.getElementById('audioBoton3');
+	objAudio.play();
+	var objBoton = document.getElementById('boton3').src="img/boton3.png";
+}
+
+console.log(GameComputer.length)
+console.log(i)
+
+setInterval (function(){
+	if (i<GameComputer.length) {
+ligthsOff();
+console.log('entro en el bucle');
+			
+		if (GameComputer[i]==0) zero ();
+		if (GameComputer[i]==1) uno ();
+		if (GameComputer[i]==2) dos ();
+		if (GameComputer[i]==3) tres ();
+		i++
+		console.log(i+' vuelta');
+		
+		console.log('valor variable despues de la primera vuelta'+i);
+} 
+	
+},500);
+
+
+console.log ('fuera del bucle')	
